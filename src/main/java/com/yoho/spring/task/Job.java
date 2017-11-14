1c7f0f7ab911e115c546afb527a7add16ac97f6e
yanzhang.fu@yoho.cn <yanzhang.fu@yoho.cn>
2017-11-14 10:19:34 +0800

    添加task学习笔记

--- src/main/java/com/yoho/spring/task/Job.java
+++ src/main/java/com/yoho/spring/task/Job.java
@@ -3,6 +3,9 @@ package com.yoho.spring.task;
 public class Job {
 
     public void process(){
+        Long begin = System.currentTimeMillis();
         System.out.println("执行这个任务。。。");
+        Long end = System.currentTimeMillis();
+
     }
 }
